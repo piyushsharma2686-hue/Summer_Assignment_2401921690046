f6e8fe818c45dfdfda4b4e6d5368f8dbd8835dc6
piyushsharma2686-hue <piyushsharma2686@gmail.com>
2026-06-02 22:24:43 +0530

    Update Create Best Time to Buy and Sell Stock.cpp

--- Week_1/Day 1/Create Best Time to Buy and Sell Stock.cpp
+++ Week_1/Day 1/Create Best Time to Buy and Sell Stock.cpp
@@ -1,1 +1,13 @@
 
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int purchase = prices[0];
+        int netpro=0;
+        for(int i=0;i<prices.size();i++){
+            purchase=min(purchase,prices[i]);
+            netpro=max(netpro,prices[i]-purchase);
+        }
+        return netpro;
+    }
+};
