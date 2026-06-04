73441eb337772eaa41e17b0c91872f72574f15fd
piyushsharma2686-hue <piyushsharma2686@gmail.com>
2026-06-04 22:11:23 +0530

    Create Squares of a sorted array.cpp

---
+++ Week_1/Day 3/Squares of a sorted array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size(),i=0,j=n-1,k=n-1;
+        vector<int> ans(n);
+
+        while(i<=j){
+            int a=nums[i]*nums[i];
+            int b=nums[j]*nums[j];
+
+            if(a>b){
+                ans[k--]=a;
+                i++;
+            }else{
+                ans[k--]=b;
+                j--;
+            }
+        }
+        return ans;
+    }
+};
