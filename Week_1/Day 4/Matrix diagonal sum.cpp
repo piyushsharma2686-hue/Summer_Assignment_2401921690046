fb52016a2bf4ce53ff0a23dbc66fcf15bb21ef34
piyushsharma2686-hue <piyushsharma2686@gmail.com>
2026-06-05 23:14:36 +0530

    Update Matrix diagonal sum.cpp

--- Week_1/Day 4/Matrix diagonal sum.cpp
+++ Week_1/Day 4/Matrix diagonal sum.cpp
@@ -1,1 +1,16 @@
- 
+ class Solution {
+public:
+    int diagonalSum(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int sum = 0;
+        for(int i=0;i<n;i++){
+            sum+=mat[i][i];
+
+            int j = n-1-i;
+            if(i!=j){
+                sum+=mat[i][j];
+            }
+        }
+        return sum;
+    }
+};
