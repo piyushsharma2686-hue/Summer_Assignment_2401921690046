c4776087ad01f2275ec926609475299b468fe8b6
piyushsharma2686-hue <piyushsharma2686@gmail.com>
2026-06-11 21:45:28 +0530

    Create Reverse Words in a String III.cpp

---
+++ Week_2/Day 4/Reverse Words in a String III.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        string ans = "";
+        string temp = "";
+        for(int i=0;i<s.length();i++){
+            
+            if(s[i]!=' '){
+                temp+=s[i];
+            }
+            else{
+                reverse(temp.begin(),temp.end());
+                temp+=' ';
+                ans+=temp;
+                temp = ""; 
+            }
+        }
+        reverse(temp.begin(),temp.end());
+        ans+=temp;
+        return ans;
+    }
+};
